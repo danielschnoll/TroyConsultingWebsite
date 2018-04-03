0c9cfbfd3fa6eed39ec7998aa11b21db1add37e4
Daniel <schnolldj@gmail.com>
2018-04-02 22:59:10 -0400

    Removed all old TCC site content

--- totransfer/resources/js/init.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  $(function(){
-  	$('.parallax').parallax();
-    $('.button-collapse').sideNav();
-    $('.collapsible').collapsible();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
